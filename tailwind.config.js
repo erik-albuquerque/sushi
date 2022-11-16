c42ad7bf55fc1538f444b286fa1a39c48ffc29a4
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2022-11-16 11:04:23 -0300

    feat: add next document + tailwindcss custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      },
+      colors: {
+        black: '#000000',
+        white: '#FFFFFF',
+        red: {
+          400: '#DD2E44'
+        },
+        grey: {
+          50: '#F2F3F5',
+          100: '#E8EAED',
+          400: '#7C7C8A',
+          600: '#4F5660',
+          700: '#333333'
+        }
+      }
+    }
   },
   plugins: []
 }
