1eca18759ee54d3676c7aaa904229380b86efaa9
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2022-11-16 22:23:55 -0300

    feat: add Logo component

---
+++ src/components/Logo/Badge.tsx
@@ -0,0 +1,14 @@
+type BadgeProps = {
+  label: 'Pre-alpha' | 'Alpha' | 'Beta'
+}
+
+const Badge: React.FC<BadgeProps> = ({ label }: BadgeProps) => {
+  return (
+    <div className="bg-gray-100 px-2 py-1 rounded-md max-w-max flex items-center cursor-default">
+      <span className="font-medium text-grey-700 text-xs">{label}</span>
+    </div>
+  )
+}
+
+export type { BadgeProps }
+export { Badge }
