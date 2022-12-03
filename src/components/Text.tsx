ba7b3676fa10610cbbd26791139974abc1252213
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2022-12-03 11:46:00 -0300

    feat(components): add Text component

---
+++ src/components/Text.tsx
@@ -0,0 +1,37 @@
+import { Slot } from '@radix-ui/react-slot'
+import { clsx } from 'clsx'
+import { HtmlHTMLAttributes, ReactNode } from 'react'
+
+type TextProps = HtmlHTMLAttributes<HTMLSpanElement> & {
+  size?: 'sm' | 'md' | 'lg'
+  children: ReactNode
+  asChild?: boolean
+  className?: string
+}
+
+const Text: React.FC<TextProps> = ({
+  size = 'md',
+  children,
+  asChild = false,
+  className
+}: TextProps) => {
+  const Comp = asChild ? Slot : 'span'
+
+  return (
+    <Comp
+      className={clsx(
+        'text-gray-600 font-sans',
+        {
+          'text-xs': size === 'sm',
+          'text-sm': size === 'md',
+          'text-md': size === 'lg'
+        },
+        className
+      )}
+    >
+      {children}
+    </Comp>
+  )
+}
+
+export { Text }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export * from './Avatar'
 export * from './Header'
+export * from './Heading'
 export * from './Logo'
+export * from './Text'
