e6998a02192da38b6a4acb39d29c3e287affbbd6
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2022-12-06 20:23:55 -0300

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,7 @@ const Document = () => {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       </Head>
       <body>
         <Main />
